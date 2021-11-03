17a458c6f7815bcea1d9f636de1dc00092d1d1ad
“Daysha <+>
2021-11-03 11:35:23 -0700

    commit tree 78abe3c24a1544921f49f1758319a521f9789dbc

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,42 @@
+class AttractionsController < ApplicationController
+
+    def index
+        @attractions = Attraction.all
+    end
+
+    def new
+        @attraction = Attraction.new
+    end
+
+    def edit
+        @attraction = Attraction.find_by(id: params[:id])
+    end
+
+    def update
+        @attraction = Attraction.find_by(id: params[:id])
+        @attraction = Attraction.update(attraction_params)
+        redirect_to :controller => 'attractions', :action => 'show'
+      end 
+      
+    def destroy
+    end 
+      
+    def show
+        @attraction = Attraction.find_by(id: params[:id])
+        @ride = @attraction.rides.build(user_id: current_user.id)
+        if session[:user_id] 
+            @user = User.find(session[:user_id])
+        end
+    end
+
+    def create
+        @attraction = Attraction.create(attraction_params)
+        redirect_to attraction_path(@attraction)
+    end 
+    
+    private 
+    
+    def attraction_params
+        params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+    end
+end
